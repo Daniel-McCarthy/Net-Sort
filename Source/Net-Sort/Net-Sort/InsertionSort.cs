3dc6c18661615e5db4f98c3f12e7dd5b838aeb25
Dan McCarthy <wererabbbit@yahoo.com>
2017-09-13 14:33:03 -0500

    Net-Sort: Added initial InsertionSort Implementation

--- Source/Net-Sort/Net-Sort/InsertionSort.cs
+++ Source/Net-Sort/Net-Sort/InsertionSort.cs
@@ -8,5 +8,38 @@ namespace Net_Sort
 {
     public static class InsertionSort
     {
+
+        public static byte[] insertionSort(byte[] data)
+        {
+            byte c = 0;
+
+            for(int i = 1; i < data.Length; i++)
+            {
+                bool swapNeeded = data[i] < data[i-1];
+
+                int j = i;
+                while(swapNeeded)
+                {
+                    
+                    c = data[j - 1];
+                    data[j - 1] = data[j];
+                    data[j] = c;
+
+                    j--;
+
+                    if (j > 0)
+                    {
+                        swapNeeded = data[j] < data[j - 1];
+                    }
+                    else
+                    {
+                        swapNeeded = false;
+                    }
+                }
+            }
+
+            return data;
+        }
+
     }
 }
