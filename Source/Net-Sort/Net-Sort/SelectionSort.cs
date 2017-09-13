5b0fa99b9f10a8aa51a3070517c3f3745fe81877
Dan McCarthy <wererabbbit@yahoo.com>
2017-09-13 00:01:03 -0500

    Net-Sort: Added initial Implementation of Selection Sort

--- Source/Net-Sort/Net-Sort/SelectionSort.cs
+++ Source/Net-Sort/Net-Sort/SelectionSort.cs
@@ -8,6 +8,39 @@ namespace Net_Sort
 {
     public static class SelectionSort
     {
+        public static byte[] selectionSort(byte[] data)
+        {
+            byte c = 0;
+            bool swapNeeded = false;
+            
+            for(int i = 0; i < data.Length; i++)
+            {
+                int closestValue = data[i];
+                int closestIndex = i;
+
+                for(int j = (i + 1); j < data.Length; j++)
+                {
+                    if(data[j] < closestValue)
+                    {
+                        closestValue = data[j];
+                        closestIndex = j;
+                        swapNeeded = true;
+                    }
+                }
+
+                if (swapNeeded)
+                {
+                    c = data[i];
+                    data[i] = data[closestIndex];
+                    data[closestIndex] = c;
+                }
+
+                swapNeeded = false;
+            }
+            
+
+            return data;
+        }
 
     }
 }
