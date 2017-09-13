bcedc05b03c20eaca9dd3ada9a57fc505137c3a4
Dan McCarthy <wererabbbit@yahoo.com>
2017-09-12 20:27:03 -0500

    Net-Sort: Added Descending Test Case

--- Source/Net-Sort/TestFramework/Program.cs
+++ Source/Net-Sort/TestFramework/Program.cs
@@ -8,6 +8,15 @@ namespace TestFramework
 {
     class Program
     {
+        byte[] descendingTest = new byte[100];
+
+        void initializeDescendingTest()
+        {
+            for (int i = 0; i < descendingTest.Length; i++)
+            {
+                descendingTest[i] = (byte)(100 - i);
+            }
+        }
         static void Main(string[] args)
         {
         }
