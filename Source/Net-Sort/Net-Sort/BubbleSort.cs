5d098cb48b351bc5c30931464f7a7b0210e33de0
Dan McCarthy <wererabbbit@yahoo.com>
2017-09-12 21:55:13 -0500

    Net-Sort: Included Initial Bubble Sort Implementation

--- Source/Net-Sort/Net-Sort/BubbleSort.cs
+++ Source/Net-Sort/Net-Sort/BubbleSort.cs
@@ -8,5 +8,34 @@ namespace Net_Sort
 {
     public static class BubbleSort
     {
+
+        public static byte[] bubbleSort(byte[] data)
+        {
+            byte c = 0;
+
+            bool hasSwapped = true;
+
+            while (hasSwapped)
+            {
+                hasSwapped = false;
+
+                for (int i = 1; i < data.Length; i++)
+                {
+
+                    if (data[i - 1] > data[i])
+                    {
+                        //Swap current entry with previous entry
+                        c = data[i - 1];
+                        data[i - 1] = data[i];
+                        data[i] = c;
+
+                        hasSwapped = true;
+                    }
+                }
+            }
+
+            return data;
+        }
+
     }
 }
