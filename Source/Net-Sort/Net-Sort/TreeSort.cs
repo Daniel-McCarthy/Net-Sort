e0eaa133cefcad6f7a995acfd3fd092c2ccfcd36
Dan McCarthy <wererabbbit@yahoo.com>
2017-09-14 15:43:50 -0500

    Net-Sort: Added TreeNode Class to Tree Sort

--- Source/Net-Sort/Net-Sort/TreeSort.cs
+++ Source/Net-Sort/Net-Sort/TreeSort.cs
@@ -10,5 +10,76 @@ namespace Net_Sort
     {
 
 
+        private class TreeNode
+        {
+            public TreeNode parentNode;
+            public TreeNode left;
+            public TreeNode right;
+
+            public bool visited = false;
+            public int duplicates = 0;
+            public byte value = 0;
+
+            public TreeNode(byte value, int arrayIndex)
+            {
+                this.value = value;
+            }
+
+            public void addNode(TreeNode node)
+            {
+                bool hasChild = false;
+                bool hitBottom = false;
+
+                TreeNode nextNode = this;
+
+                while(!hitBottom)
+                {
+                    if(node.value == nextNode.value)
+                    {
+                        nextNode.duplicates++;
+                        hitBottom = true;
+                    }
+                    else if((node.value < nextNode.value))
+                    {
+                        //Move left
+                        hasChild = (nextNode.left == null) ? false : true;
+
+                        if (!hasChild)
+                        {
+                            //Add
+                            nextNode.left = node;
+                            node.parentNode = nextNode;
+                            hitBottom = true;
+                        }
+                        else
+                        {
+                            nextNode = nextNode.left;
+                            hitBottom = false;
+                        }
+                    }
+                    else
+                    {
+                        //Move right
+                        hasChild = (nextNode.right == null) ? false : true;
+
+                        if (!hasChild)
+                        {
+                            //Add
+                            nextNode.right = node;
+                            node.parentNode = nextNode;
+                            hitBottom = true;
+                        }
+                        else
+                        {
+                            nextNode = nextNode.right;
+                            hitBottom = false;
+                        }
+                    }
+                }
+
+
+            }
+        }
+
     }
 }
