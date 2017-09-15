bc30f85bd70388bfdaf48ea962822bb6f3f8ab5d
Dan McCarthy <wererabbbit@yahoo.com>
2017-09-15 00:06:17 -0500

    Net-Sort: Included initial Implementation of Gnome Sort

--- Source/Net-Sort/Net-Sort/GnomeSort.cs
+++ Source/Net-Sort/Net-Sort/GnomeSort.cs
@@ -9,6 +9,27 @@ namespace Net_Sort
     public static class GnomeSort
     {
 
+        public static byte[] gnomeSort(byte[] data)
+        {
+            byte c = 0;
+
+            for (int i = 0; i < data.Length;)
+            {
+                if ((i == 0) || (data[i]) >= data[i - 1])
+                {
+                    i++;
+                }
+                else
+                {
+                    c = data[i];
+                    data[i] = data[i - 1];
+                    data[i - 1] = c;
+                    i--;
+                }
+            }
+
+            return data;
+        }
 
     }
 }
