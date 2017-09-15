8042bde03837d2863680d1c5689e0eafa71c195e
Dan McCarthy <wererabbbit@yahoo.com>
2017-09-14 20:12:48 -0500

    Net-Sort: Added initial BucketSort Implementation

--- Source/Net-Sort/Net-Sort/BucketSort.cs
+++ Source/Net-Sort/Net-Sort/BucketSort.cs
@@ -9,6 +9,34 @@ namespace Net_Sort
     public static class BucketSort
     {
 
+        public static byte[] bucketSort(byte[] data)
+        {
+
+            List<byte>[] buckets = new List<byte>[17];
+            for(int i = 0; i < buckets.Length; i++)
+            {
+                buckets[i] = new List<byte>();
+            }
+
+
+            for(int i = 0; i < data.Length; i++)
+            {
+                buckets[data[i] / 16].Add(data[i]);
+            }
+
+            int index = 0;
+            for(int i = 0; i < buckets.Length; i++)
+            {
+                buckets[i].Sort();
+
+                for(int j = 0; j < buckets[i].Count; j++)
+                {
+                    data[index++] = buckets[i][j];
+                }
+            }
+
+            return data;
+        }
 
     }
 }
