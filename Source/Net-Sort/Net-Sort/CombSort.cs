103f08f02bb86e4f360e969d35a75bc278975a4f
Dan McCarthy <wererabbbit@yahoo.com>
2017-09-16 19:35:51 -0500

    Net-Sort: Added combSort Function

--- Source/Net-Sort/Net-Sort/CombSort.cs
+++ Source/Net-Sort/Net-Sort/CombSort.cs
@@ -9,6 +9,29 @@ namespace Net_Sort
     public static class CombSort
     {
 
+        public static byte[] combSort(byte[] data)
+        {
+            int combWidth = 19;
+            byte c = 0;
+
+            while(combWidth > 0)
+            {
+                int i = 0;
+                while ((i + combWidth) < data.Length)
+                {
+                    if(data[i] > data[i + combWidth])
+                    {
+                        c = data[i];
+                        data[i] = data[i + combWidth];
+                        data[i + combWidth] = c;
+                    }
+                    i++;
+                }
+                combWidth--;
+            }
+
+            return data;
+        }
 
     }
 }
