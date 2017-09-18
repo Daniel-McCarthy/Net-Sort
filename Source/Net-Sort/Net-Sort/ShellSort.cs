1057d69f251b1abf21d62df66b2cc152efc5ac7a
Dan McCarthy <wererabbbit@yahoo.com>
2017-09-18 01:21:11 -0500

    Net-Sort: Added Shell Sort Function

--- Source/Net-Sort/Net-Sort/ShellSort.cs
+++ Source/Net-Sort/Net-Sort/ShellSort.cs
@@ -9,6 +9,32 @@ namespace Net_Sort
     public static class ShellSort
     {
 
+        public static byte[] shellSort(byte[] data)
+        {
+            byte c = 0;
+            int gap = data.Length / 2;
+
+            while(gap > 0)
+            {
+                for(int i = 0; (i + gap) < data.Length; i++)
+                {
+                    int j = (gap + i);
+                    c = data[(gap + i)];
+
+                    while (j >= gap && c < data[j - gap])
+                    {
+                        data[j] = data[j - gap];
+                        j -= gap;
+                    }
+
+                    data[j] = c;
+                }
+
+                gap /= 2;
+            }
+
+            return data;
+        }
 
     }
 }
