501292ac55231c1314618c0a1a32062621fe0506
Dan McCarthy <wererabbbit@yahoo.com>
2017-09-18 14:05:53 -0500

    Net-Sort: Added oddEven Sort Function

--- Source/Net-Sort/Net-Sort/OddEvenSort.cs
+++ Source/Net-Sort/Net-Sort/OddEvenSort.cs
@@ -9,6 +9,29 @@ namespace Net_Sort
     public static class OddEvenSort
     {
 
+        public static byte[] oddEvenSort(byte[] data)
+        {
+            byte c = 0;
+
+            for (int i = 0; i < data.Length; i++)
+            {
+                int j = ((i % 2) == 0) ? 2 : 1;
+
+                while(j < data.Length)
+                {
+                    if (data[j] < data[j - 1])
+                    {
+                        c = data[j];
+                        data[j] = data[j - 1];
+                        data[j - 1] = c;
+                    }
+
+                    j += 2;
+                }
+            }
+
+            return data;
+        }
 
     } 
 }
